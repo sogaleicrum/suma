3aa4511d3e5e7e80cc6eeabcb2d3bbf1f9373499
sogaleicrum <50418130+sogaleicrum@users.noreply.github.com>
2024-04-21 23:46:03 -0600

    my girst commit

---
+++ suma de dos numeros/src/suma/de/dos/numeros/SumaDeDosNumeros.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma.de.dos.numeros;
+import java.util.*;
+/**
+ *
+ * @author Jason
+ */
+public class SumaDeDosNumeros {
+
+    /**
+     * RP21005 JASON ISAAC RODRIGUEZ PEREZ
+     */
+    public static void main(String[] args) {
+      int num1, num2, sum;
+      Scanner s = new Scanner(System.in);
+        System.out.println("escribe el primer nuemro");
+        num1= s.nextInt();
+        System.out.println("escribe el segundo nuemro");
+        num2= s.nextInt();
+        sum = num1+num2;
+        System.out.println("La suma de los nuemero es: "+ sum);
+      
+    }
+    
+}
